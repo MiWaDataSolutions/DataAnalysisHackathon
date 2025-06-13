f99c40f86773b95bae5daed40afa921c3200451a
Michael Rademeyer <michaelrademeyer@hotmail.com>
2025-06-14 00:02:21 +0200

    #1 Handle Data Sessions List
    - Completed backend component

---
+++ backend/DataAnalysisHackathonBackend/Interfaces/Services/IDataSessionService.cs
@@ -0,0 +1,17 @@
+﻿using DataAnalysisHackathonBackend.Models;
+
+namespace DataAnalysisHackathonBackend.Interfaces.Services
+{
+    public interface IDataSessionService
+    {
+        Task<List<DataSession>> GetDataSessionsAsync(string userId);
+
+        Task<DataSession> GetDataSessionAsync(Guid dataSessionId, string userId);
+
+        Task<Guid> CreateDataSession(DataSession dataSession, string userId);
+
+        Task DeleteDataSessionAsync(Guid dataSessionId, string userId);
+
+        Task UpdateDataSession(Guid dataSessionId, string dataSessionName, string userId);
+    }
+}
---
+++ backend/DataAnalysisHackathonBackend/Migrations/20250613211816_AddDataSession.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAnalysisHackathonBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DataSessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "GoogleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataSessions_UserId",
+                table: "DataSessions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataSessions");
+        }
+    }
+}
